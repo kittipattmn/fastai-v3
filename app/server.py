58b63cc40aaa5421da854ecb33b59b14e6760cc0
kittipattmn <54210011+kittipattmn@users.noreply.github.com>
2020-03-19 10:19:10 +0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/4u7d966odlevqn0/export-malaria.pkl?dl=1' # edit this
+export_file_name = 'export-malaria.pkl' # edit this
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Parasitized', 'Uninfected'] # edit this
 path = Path(__file__).parent
 
 app = Starlette()
